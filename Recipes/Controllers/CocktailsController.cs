fba7c0ccde317d63f4a0bb48123457bbc2601069
TeamTsundereNat <115222955+matheoDieryck@users.noreply.github.com>
2024-03-07 14:53:48 +0100

    model Cocktail added

--- Recipes/Controllers/CocktailsController.cs
+++ Recipes/Controllers/CocktailsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Recipes.Controllers
 {
@@ -8,5 +9,52 @@ namespace Recipes.Controllers
         {
             return View();
         }
+        public async Task<ActionResult> RandomCocktail()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<dynamic>(jsonString);
+                    var cocktail = data.drinks[0];
+                    Cocktail model = new Cocktail
+                    {
+                        IdDrink = cocktail.idDrink,
+                        StrDrink = cocktail.strDrink,
+                        StrDrinkAlternate = cocktail.strDrinkAlternate,
+                        StrTags = cocktail.strTags,
+                        StrVideo = cocktail.strVideo,
+                        StrCategory = cocktail.strCategory,
+                        StrIBA = cocktail.strIBA,
+                        StrAlcoholic = cocktail.strAlcoholic,
+                        StrGlass = cocktail.strGlass,
+                        StrInstructions = cocktail.strInstructions,
+                        StrDrinkThumb = cocktail.strDrinkThumb,
+                        StrIngredients = new List<string>(),
+                        StrMeasures = new List<string>(),
+                        StrCreativeCommonsConfirmed = cocktail.strCreativeCommonsConfirmed,
+                        DateModified = cocktail.dateModified
+                    };
+
+                    for (int i = 1; i <= 15; i++)
+                    {
+                        var ingredient = cocktail[$"strIngredient{i}"];
+                        var measure = cocktail[$"strMeasure{i}"];
+                        if (ingredient != null) model.StrIngredients.Add(ingredient.Value);
+                        if (measure != null) model.StrMeasures.Add(measure.Value);
+                    }
+
+                    return View(model);
+                }
+                else
+                {
+                    return View("Error");
+                }
+            }
+        }
+
     }
 }
+
