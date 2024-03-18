5420cc5a8cd858abbee43397f94109fb2819681a
discedric <cedric.vaneessen07@gmail.com>
2024-03-18 14:59:52 +0100

    create login and profile controller

---
+++ Recipes/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recipes.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
